04726a2021e66a3c663638e08ac6148e7efd244a
Anthony Maras <marasantonis@gmail.com>
2022-12-30 11:27:24 +0200

    set up app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Navbar, About } from './components/index';
 
-const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  return (
+    <main className="grid">
+      <section className="grid__about">
+        <Navbar />
+        <About />
+      </section>
+      <section className="grid__skills">Skills</section>
+      <section className="grid__projects">Projects</section>
+      <section className="grid__contact">
+        <Navbar />
+      </section>
+    </main>
+  );
+}
 
 export default App;
