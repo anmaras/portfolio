cacb71cfd1030d14e6488e4bcf6bc336ba9cb6e2
Anthony Maras <marasantonis@gmail.com>
2023-01-02 19:08:19 +0200

    add project objs

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import { FaGitAlt } from 'react-icons/fa';
+import { FiFigma } from 'react-icons/fi';
+import { SiVite, SiWebpack } from 'react-icons/si';
+import audioSmall from '../assets/audio-small.webp';
+import audioLarge from '../assets/audio-large.webp';
+import memorySmall from '../assets/memory-small.webp';
+import memoryLarge from '../assets/memory-large.webp';
 
 import {
   DiCss3,
@@ -10,10 +17,6 @@ import {
   DiVisualstudio,
   DiPhotoshop,
 } from 'react-icons/di';
-
-import { FaGitAlt } from 'react-icons/fa';
-import { FiFigma } from 'react-icons/fi';
-import { SiVite, SiWebpack } from 'react-icons/si';
 
 export const skills = [
   { iconUrl: <DiCss3 />, icon: 'CSS', id: 0 },
@@ -28,4 +31,51 @@ export const skills = [
   { iconUrl: <FiFigma />, icon: 'Figma', id: 9 },
   { iconUrl: <SiVite />, icon: 'ViteJs', id: 10 },
   { iconUrl: <SiWebpack />, icon: 'Webpack', id: 11 },
+];
+
+export const projects = [
+  {
+    title: 'e-commerce website',
+    tech: [
+      'React',
+      'Sass',
+      'Axios',
+      'Formik',
+      'Yup',
+      'Firebase',
+      'React Query',
+      'React Router',
+      'Framer Motion',
+      'Vite js',
+    ],
+    description:
+      'An e-commerce website which allow users to view different audio related products.',
+    urlSmall: audioSmall,
+    urlBig: audioLarge,
+    githubUrl: 'https://github.com/anmaras/audio-ecommerce-website',
+    liveUrl: 'https://ecommerce-antonis-maras.netlify.app/',
+    id: 0,
+  },
+  {
+    title: 'memory game',
+    tech: ['React', 'CSS', 'Framer Motion', 'Webpack'],
+    description:
+      'A game platform that helps you keep your memory sharp while trying to match pairs of icons or numbers.',
+    urlSmall: memorySmall,
+    urlBig: memoryLarge,
+    githubUrl: 'https://github.com/anmaras/memory-game',
+    liveUrl: 'https://antonis-maras-memory.netlify.app/',
+    id: 1,
+  },
+  // {
+  //   title: 'weather app',
+  //   tech: ['html', 'CSS', 'javascript'],
+  //   description:
+  //     'A weather application that provides current, hourly and daily forecast weather data.',
+  //   urlSmall: memorySmall,
+  //   urlBig: memoryLarge,
+  //   githubUrl: 'https://github.com/anmaras/weather-app',
+  //   liveUrl: 'https://antonis-maras-weather-app.netlify.app/',
+  //   id: 1,
+  // },
 ];
