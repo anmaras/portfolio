0ee34c769c5d9602919cb59e8a6f84cf00538664
Anthony Maras <marasantonis@gmail.com>
2023-01-03 15:08:51 +0200

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import { AiFillFile, AiFillGithub, AiFillLinkedin } from 'react-icons/Ai';
 import style from './Navbar.module.scss';
 import imgUrl from '../../assets/cv.pdf';
+import PropTypes from 'prop-types';
 
-const Navbar = () => {
+const Navbar = ({ position }) => {
   return (
-    <nav className={style.navbar}>
+    <nav className={position ? style.navbar : style['navbar--bottom']}>
       <div className={style['navbar__wrapper']}>
         <h4 className={[style['navbar__name'], 'headingS'].join(' ')}>
           antonismaras
         </h4>
-        <div className={style['navbar__iconsWrapper']}>
+        <div
+          className={
+            position
+              ? style['navbar__iconsWrapper']
+              : style['navbar__iconsWrapper--bottom']
+          }
+        >
           <a href="https://github.com/anmaras" target="_blank" rel="noreferrer">
             <AiFillGithub className={style['navbar__icon']} size={25} />
           </a>
@@ -24,6 +31,10 @@ const Navbar = () => {
       </div>
     </nav>
   );
+};
+
+Navbar.propTypes = {
+  position: PropTypes.bool.isRequired,
 };
 
 export default Navbar;
