62284bcd66077c7fdd7468fc058d0bbab46ea661
Anthony Maras <marasantonis@gmail.com>
2023-01-03 15:09:13 +0200

    update index

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,5 +3,6 @@ import About from './About/About';
 import Button from './Button/Button';
 import Skills from './Skills/Skills';
 import Projects from './Projects/Projects';
+import Contact from './Contact/Contact';
 
-export { Navbar, About, Button, Skills, Projects };
+export { Navbar, About, Button, Skills, Projects, Contact };
