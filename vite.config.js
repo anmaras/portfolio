ac0d29c84f7d5eec76c60fa77f937e39b4835201
Anthony Maras <marasantonis@gmail.com>
2023-01-17 17:45:19 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
-  base: 'https://github.com/anmaras/Portfolio',
+  base: '/Portfolio/',
   plugins: [react()],
   css: {
     postcss: {
