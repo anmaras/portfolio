158df35b234d798ba37d83e9c4e7c278f18ab08c
Anthony Maras <marasantonis@gmail.com>
2023-02-05 22:43:16 +0200

    add extra icons and set up button list

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { FaGitAlt } from 'react-icons/fa';
+import { FaGitAlt, FaNode } from 'react-icons/fa';
 import { FiFigma } from 'react-icons/fi';
-import { SiVite, SiWebpack } from 'react-icons/si';
+import { SiVite, SiWebpack, SiExpress, SiMongodb } from 'react-icons/si';
 import audioSmall from '../assets/audio-small.webp';
 import audioLarge from '../assets/audio-large.webp';
 import memorySmall from '../assets/memory-small.webp';
@@ -26,13 +26,16 @@ export const skills = [
   { iconUrl: <DiJavascript1 />, icon: 'JavaScript', id: 2 },
   { iconUrl: <DiReact />, icon: 'React', id: 3 },
   { iconUrl: <DiSass />, icon: 'Sass', id: 4 },
-  { iconUrl: <FaGitAlt />, icon: 'Git', id: 5 },
-  { iconUrl: <DiNpm />, icon: 'Npm', id: 6 },
-  { iconUrl: <DiVisualstudio />, icon: 'Visual Code', id: 7 },
-  { iconUrl: <DiPhotoshop />, icon: 'Photoshop', id: 8 },
-  { iconUrl: <FiFigma />, icon: 'Figma', id: 9 },
-  { iconUrl: <SiVite />, icon: 'ViteJs', id: 10 },
-  { iconUrl: <SiWebpack />, icon: 'Webpack', id: 11 },
+  { iconUrl: <FaNode />, icon: 'NodeJs', id: 5 },
+  { iconUrl: <SiExpress />, icon: 'Express', id: 6 },
+  { iconUrl: <SiMongodb />, icon: 'MongoDB', id: 7 },
+  { iconUrl: <FaGitAlt />, icon: 'Git', id: 8 },
+  { iconUrl: <DiNpm />, icon: 'Npm', id: 9 },
+  { iconUrl: <DiVisualstudio />, icon: 'Visual Code', id: 10 },
+  { iconUrl: <DiPhotoshop />, icon: 'Photoshop', id: 11 },
+  { iconUrl: <FiFigma />, icon: 'Figma', id: 12 },
+  { iconUrl: <SiVite />, icon: 'ViteJs', id: 13 },
+  { iconUrl: <SiWebpack />, icon: 'Webpack', id: 14 },
 ];
 
 export const projects = [
@@ -80,4 +83,37 @@ export const projects = [
   //   liveUrl: 'https://antonis-maras-weather-app.netlify.app/',
   //   id: 3,
   // },
+];
+
+export const aboutButtons = [
+  {
+    text: 'projects',
+    id: 1,
+    element: () => {
+      const contact = document.getElementById('projects');
+      if (contact) {
+        contact.scrollIntoView();
+      }
+    },
+  },
+  {
+    text: 'skills',
+    id: 2,
+    element: () => {
+      const contact = document.getElementById('skills');
+      if (contact) {
+        contact.scrollIntoView();
+      }
+    },
+  },
+  {
+    text: 'contact',
+    id: 3,
+    element: () => {
+      const contact = document.getElementById('contact');
+      if (contact) {
+        contact.scrollIntoView();
+      }
+    },
+  },
 ];
