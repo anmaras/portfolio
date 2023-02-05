87492c5c57fa596e19e55fabcdeaeb47af094c2b
Anthony Maras <marasantonis@gmail.com>
2023-02-05 22:42:42 +0200

    add drag n drop

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -3,34 +3,46 @@ import { skills } from '../../utils/constants';
 import style from './Skills.module.scss';
 import { IconContext } from 'react-icons/lib';
 import { motion } from 'framer-motion';
+import { useRef } from 'react';
 
 const Skills = () => {
+  const constraintsRef = useRef(null);
   return (
-    <ul className={style.list}>
-      {skills.map((skill, index) => {
-        const { iconUrl, id, icon } = skill;
-        return (
-          <motion.li
-            key={id}
-            className={style['list__item']}
-            whileHover={{ scale: 1.2 }}
-            transition={{ type: 'spring', stiffness: 800, damping: 10 }}
-            initial={{ opacity: 0 }}
-            animate={{
-              opacity: 1,
-              transition: { delay: 0.1 * index },
-            }}
-          >
-            <IconContext.Provider
-              value={{ className: `${style['list__img']}`, size: 70 }}
+    <>
+      <h2 className="headingXL">Skills</h2>
+      <ul ref={constraintsRef} className={style.list}>
+        {skills.map((skill, index) => {
+          const { iconUrl, id, icon } = skill;
+          return (
+            <motion.li
+              key={id}
+              drag
+              dragConstraints={{
+                top: 0,
+                left: 0,
+                right: 0,
+                bottom: 0,
+              }}
+              className={style['list__item']}
+              whileHover={{ scale: 1.2 }}
+              transition={{ type: 'spring', stiffness: 800, damping: 10 }}
+              initial={{ opacity: 0 }}
+              animate={{
+                opacity: 1,
+                transition: { delay: 0.1 * index },
+              }}
             >
-              {iconUrl}
-            </IconContext.Provider>
-            <p className={[style['list__text']].join(' ')}>{icon}</p>
-          </motion.li>
-        );
-      })}
-    </ul>
+              <IconContext.Provider
+                value={{ className: `${style['list__img']}`, size: 70 }}
+              >
+                {iconUrl}
+              </IconContext.Provider>
+              <p className={[style['list__text']].join(' ')}>{icon}</p>
+            </motion.li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
 
