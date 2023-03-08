2584d8c2256450d22d91815be4895291ac78c5fa
Anthony Maras <marasantonis@gmail.com>
2023-03-08 21:59:46 +0200

    constants update

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -6,8 +6,9 @@ import audioSmall from '../assets/audio-small.webp';
 import audioLarge from '../assets/audio-large.webp';
 import memorySmall from '../assets/memory-small.webp';
 import memoryLarge from '../assets/memory-large.webp';
-// import weatherSmall from '../assets/weather-small.webp';
-// import weatherLarge from '../assets/weather-large.webp';
+import weatherSmall from '../assets/kanban-mobile.jpg';
+import weatherLarge from '../assets/kanban-large.jpg';
+import { ReactComponent as MongooseIcon } from '../assets/mongoose.svg';
 
 import {
   DiCss3,
@@ -28,30 +29,41 @@ export const skills = [
   { iconUrl: <DiSass />, icon: 'Sass', id: 4 },
   { iconUrl: <FaNode />, icon: 'NodeJs', id: 5 },
   { iconUrl: <SiExpress />, icon: 'Express', id: 6 },
-  { iconUrl: <SiMongodb />, icon: 'MongoDB', id: 7 },
-  { iconUrl: <FaGitAlt />, icon: 'Git', id: 8 },
+  { iconUrl: <MongooseIcon />, icon: 'Mongoose', id: 7 },
+  { iconUrl: <SiMongodb />, icon: 'MongoDB', id: 8 },
   { iconUrl: <DiNpm />, icon: 'Npm', id: 9 },
   { iconUrl: <DiVisualstudio />, icon: 'Visual Code', id: 10 },
   { iconUrl: <DiPhotoshop />, icon: 'Photoshop', id: 11 },
   { iconUrl: <FiFigma />, icon: 'Figma', id: 12 },
   { iconUrl: <SiVite />, icon: 'ViteJs', id: 13 },
   { iconUrl: <SiWebpack />, icon: 'Webpack', id: 14 },
+  { iconUrl: <FaGitAlt />, icon: 'Git', id: 15 },
 ];
 
 export const projects = [
   {
-    title: 'e-commerce website',
+    title: 'Kanban',
+    tech: ['React', 'SCSS', 'Node', 'Express', 'Mongoose', 'Mongo-DB'],
+    description:
+      'A full stack app that enables users to create boards with customizable columns and tasks, representing different stages of their work process.',
+    urlSmall: weatherSmall,
+    urlBig: weatherLarge,
+    githubUrl: 'https://github.com/anmaras/kanban-task-management-app',
+    liveUrl: 'https://kanban-task-management-app.onrender.com/',
+    id: 0,
+  },
+  {
+    title: 'audiophile',
     tech: [
       'React',
-      'Sass',
+      'SCSS',
+      'React Router',
+      'React Query',
       'Axios',
+      'Firebase ',
+      'Framer-Motion',
       'Formik',
       'Yup',
-      'Firebase',
-      'React Query',
-      'React Router',
-      'Framer Motion',
-      'Vite js',
     ],
     description:
       'An e-commerce website which allow users to view different audio related products.',
@@ -59,33 +71,32 @@ export const projects = [
     urlBig: audioLarge,
     githubUrl: 'https://github.com/anmaras/audio-ecommerce-website',
     liveUrl: 'https://ecommerce-antonis-maras.netlify.app/',
-    id: 0,
+    id: 1,
   },
   {
-    title: 'memory game',
-    tech: ['React', 'CSS', 'Framer Motion', 'Webpack'],
+    title: 'memory',
+    tech: ['React', 'CSS', 'Framer-Motion'],
     description:
       'A game platform that helps you keep your memory sharp while trying to match pairs of icons or numbers.',
     urlSmall: memorySmall,
     urlBig: memoryLarge,
     githubUrl: 'https://github.com/anmaras/memory-game',
     liveUrl: 'https://antonis-maras-memory.netlify.app/',
-    id: 1,
+    id: 2,
   },
-  // {
-  //   title: 'weather app',
-  //   tech: ['html', 'CSS', 'javascript'],
-  //   description:
-  //     'A weather application that provides current, hourly and daily forecast weather data.',
-  //   urlSmall: weatherSmall,
-  //   urlBig: weatherLarge,
-  //   githubUrl: 'https://github.com/anmaras/weather-app',
-  //   liveUrl: 'https://antonis-maras-weather-app.netlify.app/',
-  //   id: 3,
-  // },
 ];
 
 export const aboutButtons = [
+  {
+    text: 'skills',
+    id: 0,
+    element: () => {
+      const contact = document.getElementById('skills');
+      if (contact) {
+        contact.scrollIntoView();
+      }
+    },
+  },
   {
     text: 'projects',
     id: 1,
@@ -97,18 +108,8 @@ export const aboutButtons = [
     },
   },
   {
-    text: 'skills',
-    id: 2,
-    element: () => {
-      const contact = document.getElementById('skills');
-      if (contact) {
-        contact.scrollIntoView();
-      }
-    },
-  },
-  {
     text: 'contact',
-    id: 3,
+    id: 2,
     element: () => {
       const contact = document.getElementById('contact');
       if (contact) {
