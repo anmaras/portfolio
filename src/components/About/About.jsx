17f1e3653af11cabc3713a5362bc0d1b13482b02
Antonis Maras <95342891+anmaras@users.noreply.github.com>
2025-06-07 21:14:05 +0300

    feat: Greetings text change

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import style from './About.module.scss';
-import Button from '../Button/Button';
-import mobile from '../../assets/image-profile-mobile.webp';
-import tablet from '../../assets/image-profile-tablet.webp';
-import desktop from '../../assets/image-profile-desktop.jpg';
-import { motion } from 'framer-motion';
-import { aboutButtons } from '../../utils/constants';
+import React from "react";
+import style from "./About.module.scss";
+import Button from "../Button/Button";
+import mobile from "../../assets/image-profile-mobile.webp";
+import tablet from "../../assets/image-profile-tablet.webp";
+import desktop from "../../assets/image-profile-desktop.jpg";
+import { motion } from "framer-motion";
+import { aboutButtons } from "../../utils/constants";
 
 const textVariant = {
   initial: {
-    x: '-100%',
+    x: "-100%",
   },
   animate: {
     x: 0,
     transition: {
-      when: 'beforeChildren',
+      when: "beforeChildren",
       duration: 0.65,
-      ease: 'anticipate',
+      ease: "anticipate",
       delay: 0.5,
     },
   },
@@ -29,8 +29,8 @@ const buttonVariant = {
   animate: {
     opacity: 1,
     transition: {
-      type: 'tween',
-      ease: 'circOut',
+      type: "tween",
+      ease: "circOut",
       duration: 0.5,
       staggerChildren: 0.2,
     },
@@ -52,7 +52,7 @@ const pictureVariant = {
   animate: {
     y: -30,
     transition: {
-      type: 'spring',
+      type: "spring",
       duration: 1.5,
     },
   },
@@ -62,7 +62,7 @@ const About = () => {
   return (
     <div className={style.title}>
       <motion.picture
-        className={style['title__img']}
+        className={style["title__img"]}
         initial="initial"
         animate="animate"
         variants={pictureVariant}
@@ -73,21 +73,23 @@ const About = () => {
         <img src={mobile} alt="my picture" />
       </motion.picture>
       <motion.div
-        className={style['title__textWrapper']}
+        className={style["title__textWrapper"]}
         initial="initial"
         animate="animate"
         variants={textVariant}
       >
-        <h1 className={[style['title__text'], 'headingXL'].join(' ')}>
-          {`Nice to meet you! I'm`} <span>{`Antonis Maras`}</span>.
+        <h1 className={[style["title__text"], "headingXL"].join(" ")}>
+          {`Nice to meet you!`}
+          <span>{`My name is`}</span>
+          <span>{`Antonis Maras`}</span>.
         </h1>
-        <p className={style['title__subtitle']}>
+        <p className={style["title__subtitle"]}>
           {`Based in Athens, I'm a frontend developer passionate about building
         accessible websites and web apps that users love.`}
         </p>
 
         <motion.ul
-          className={style['title__buttonWrapper']}
+          className={style["title__buttonWrapper"]}
           variants={buttonVariant}
         >
           {aboutButtons.map((button) => {
