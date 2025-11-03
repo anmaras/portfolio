59e333bd3d596399020212174f221456aae8018d
Antonis Maras <95342891+anmaras@users.noreply.github.com>
2025-11-03 23:25:35 +0200

    feat: Adds new skills and cv

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { AiFillFile, AiFillGithub, AiFillLinkedin } from 'react-icons/Ai';
-import style from './Navbar.module.scss';
-import PropTypes from 'prop-types';
-import { motion } from 'framer-motion';
+import React from "react";
+import { AiFillFile, AiFillGithub, AiFillLinkedin } from "react-icons/Ai";
+import style from "./Navbar.module.scss";
+import PropTypes from "prop-types";
+import { motion } from "framer-motion";
 
 const Navbar = ({ position }) => {
   return (
-    <nav className={position ? style.navbar : style['navbar--bottom']}>
-      <div className={style['navbar__wrapper']}>
-        <h4 className={[style['navbar__name'], 'headingS'].join(' ')}>
+    <nav className={position ? style.navbar : style["navbar--bottom"]}>
+      <div className={style["navbar__wrapper"]}>
+        <h4 className={[style["navbar__name"], "headingS"].join(" ")}>
           antonismaras
         </h4>
         <div
           className={
             position
-              ? style['navbar__iconsWrapper']
-              : style['navbar__iconsWrapper--bottom']
+              ? style["navbar__iconsWrapper"]
+              : style["navbar__iconsWrapper--bottom"]
           }
         >
           <motion.a
@@ -24,9 +24,9 @@ const Navbar = ({ position }) => {
             rel="noreferrer"
             title="GitHub"
             whileHover={{ scale: 1.2 }}
-            transition={{ type: 'spring', stiffness: 800, damping: 10 }}
+            transition={{ type: "spring", stiffness: 800, damping: 10 }}
           >
-            <AiFillGithub className={style['navbar__icon']} size={25} />
+            <AiFillGithub className={style["navbar__icon"]} size={25} />
           </motion.a>
           <motion.a
             href="https://www.linkedin.com/in/antonis-maras-20672220a/"
@@ -34,19 +34,19 @@ const Navbar = ({ position }) => {
             rel="noreferrer"
             title="LinkedIn"
             whileHover={{ scale: 1.2 }}
-            transition={{ type: 'spring', stiffness: 800, damping: 10 }}
+            transition={{ type: "spring", stiffness: 800, damping: 10 }}
           >
-            <AiFillLinkedin className={style['navbar__icon']} size={25} />
+            <AiFillLinkedin className={style["navbar__icon"]} size={25} />
           </motion.a>
           <motion.a
-            href="https://drive.google.com/file/d/18EQJNNzdLD9vo8JWQTfGg52AtSxPj4Gl/view?usp=share_link"
+            href="https://drive.google.com/file/d/167MkwvM-B8AaBcmezMQJAshp4C-5em_H/view?usp=drive_link"
             title="CV"
             rel="noreferrer"
             target="_blank"
             whileHover={{ scale: 1.2 }}
-            transition={{ type: 'spring', stiffness: 800, damping: 10 }}
+            transition={{ type: "spring", stiffness: 800, damping: 10 }}
           >
-            <AiFillFile className={style['navbar__icon']} size={25} />
+            <AiFillFile className={style["navbar__icon"]} size={25} />
           </motion.a>
         </div>
       </div>
